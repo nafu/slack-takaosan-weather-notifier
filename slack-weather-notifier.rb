d05260f903fcd61433e121ff010ecee8d7d4ee66
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-05-10 14:43:22 +0900

    Update slack-weather-notifier to post weather of tokyo

--- slack-weather-notifier.rb
+++ slack-weather-notifier.rb
@@ -2,21 +2,33 @@ require 'json'
 require 'open-uri'
 require 'slack/incoming/webhooks'
 
-uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=400010'
+def post_forecast(title, link, text, forecast)
+  attachments = [{
+    title: title,
+    title_link: link,
+    text: text,
+    image_url: forecast['image']['url'],
+    color: '#7CD197'
+  }]
+  post_text = "#{forecast['date']}の#{title}は「#{forecast['telop']}」です。\n
+  高尾山のある八王子市のピンポイント天気はこちらから
+  -> http://weather.livedoor.com/area/forecast/1320100"
 
-res     = JSON.load(open(uri).read)
-title   = res['title']
-text    = res['description']['text']
-link    = res['link']
-weather = res['forecasts'].first
+  slack = Slack::Incoming::Webhooks.new(ENV['WEBHOOK_URL'])
+  slack.post(post_text, attachments: attachments)
+end
 
-attachments = [{
-  title: title,
-  title_link: link,
-  text: text,
-  image_url: weather['image']['url'],
-  color: "#7CD197"
-}]
+def call
+  uri = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
 
-slack = Slack::Incoming::Webhooks.new ENV['WEBHOOK_URL']
-slack.post "#{weather['date']}の#{title}は「#{weather['telop']}」です。", attachments: attachments
+  res     = JSON.load(open(uri).read)
+  title   = res['title']
+  text    = res['description']['text']
+  link    = res['link']
+
+  res['forecasts'].each do |f|
+    post_forecast(title, link, text, f)
+  end
+end
+
+call
